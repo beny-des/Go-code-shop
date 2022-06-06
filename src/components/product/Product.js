6ec097fd0ed977fe0d2ac8066e22a57b04a36a70
Beny Desta <benydesta@gmail.com>
2022-06-06 08:58:38 +0300

    Go code shop

---
+++ src/components/product/Product.js
@@ -0,0 +1,15 @@
+import "./Product.css"
+const Product = ({ image, title, price })=>
+{return(
+  <div className="product-card">
+      <div className="product-image">
+        <img src={image} alt='pic' />
+      </div>
+      {console.log(image)}
+      <div className="product-info">
+        <h5>{title}</h5>
+        <h6>{price}</h6>
+      </div>
+    </div>
+  )};
+  export default Product;
---
+++ src/components/products/Products.js
@@ -0,0 +1,17 @@
+import "./Products.css";
+import Product from "../product/Product";
+const Products = ({ products }) => {
+    return (
+        <section className="products">
+      {products.map((product) => {
+        return <Product
+          key={product.id}
+          image={product.image}
+          title={product.title}
+          price={product.price}
+        />
+      })};
+    </section>
+  );
+};
+export default Products;
